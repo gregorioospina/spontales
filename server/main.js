c820905bec1523c6b9a7bd3894665889aa5d11dd
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-10-10 16:45:17 -0500

    Lang meta fix for html tag

--- server/main.js
+++ server/main.js
@@ -1,3 +1,6 @@
 import { Meteor } from "meteor/meteor";
+import { WebApp } from "meteor/webapp";
 
-Meteor.startup(() => {});
+Meteor.startup(() => {
+  WebApp.addHtmlAttributeHook(() => ({ lang: "en" }));
+});
