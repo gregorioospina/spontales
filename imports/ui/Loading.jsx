2ceb85e3f0536abb2bd982e106d3e6d82b1e257c
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-10-10 20:51:22 -0500

    Loading page centered

--- imports/ui/Loading.jsx
+++ imports/ui/Loading.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Loading.css";
 
 const Loading = () => {
   return (
     <div className="container" id="loading-container">
       <div>
-        <h2> Waiting for all the brave warriors to join battle </h2>
+        <h1 id="loading-text">
+          Waiting for all the brave warriors to join battle
+        </h1>
       </div>
       <div className="spinner">
         <div className="dot1"></div>
