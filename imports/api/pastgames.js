f55308f5d5ab235a6d0bfbb8c4a1018b4169cebc
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-10-10 20:37:48 -0500

    Past games in Menu

--- imports/api/pastgames.js
+++ imports/api/pastgames.js
@@ -3,6 +3,12 @@ import { Meteor } from "meteor/meteor";
 
 export const Pastgames = new Mongo.Collection("pastgames");
 
+if (Meteor.isServer) {
+  Meteor.publish("pastgames", () => {
+    return Pastgames.find({});
+  });
+}
+
 Meteor.methods({
   "pastgames.insert"(name, story) {
     Pastgames.upsert({ name, story }, { name, story });
